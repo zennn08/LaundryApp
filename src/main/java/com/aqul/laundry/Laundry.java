86cd8073db53f02a1fb7bd20bebb34eea33dd2ec
zennn08 <am.fadwa15@gmail.com>
2024-11-03 21:03:34 +0700

    init project

---
+++ src/main/java/com/aqul/laundry/Laundry.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.aqul.laundry;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Laundry {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }   
+}
